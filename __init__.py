7c40215b83ab50c1592dfd604d4706ef0e7f27ac
MA86 <micro.access@hotmail.com>
2022-10-29 01:35:36 -0400

    Update init.py.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # TODO: Must Regularly Update
 
 # Modules in this package so far
-__all__ = ["actor", "game", "vector2d"]
+__all__ = ["actor", "game", "vector2d", "anim_sprite_component",
+           "bg_sprite_component", "component", "ship", "sprite_component"]
